7b5a3622e28424424e62e094ca23d78734f11973
Kosuke-Sugimoto <sk12852019@outlook.jp>
2024-01-22 00:29:49 +0900

    add related to making data

--- src/datasets/IADataset.py
+++ src/datasets/IADataset.py
@@ -1,6 +1,13 @@
+from munch import Munch
 from torch.utils.data import Dataset, DataLoader
 
 class IADataset(Dataset):
+    """
+    要件：
+        画像データセットと音声データセットとでドメインごとのデータ数を揃える
+        ドメイン⇒男・女
+        今回は圧倒的に少ないと思われる音声に合わせるが別verもすぐに導入できるようにしたい
+    """
     
     def __init__(self):
-        pass
+        self.row_data: Munch = get_row_data()
